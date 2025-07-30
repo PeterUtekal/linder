a100b043b255e7cfe6bde8342fd86beb0b80d63b
Peter Utekal <peter@aiapply.co>
2025-07-30 17:35:44 +0200

    Remove deprecated files and configurations from the Laravel project, including .editorconfig, .env.example, .gitattributes, .gitignore, Dockerfile, and various application files. This cleanup prepares the project for a more streamlined development environment.

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProfileController;
-
-Route::post('/profiles', [ProfileController::class, 'store']);
-Route::get('/profiles/{slug}', [ProfileController::class, 'show']);
-Route::post('/profiles/{slug}/swipe', [ProfileController::class, 'swipe']);
